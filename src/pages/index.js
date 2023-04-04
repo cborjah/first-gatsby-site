ef350e069543f415c2f9e088f9118f4e24d36d0e
Christian Borja <cpborja32@gmail.com>
2023-04-04 16:28:14 -0700

    adds navigation links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 // Step 1: Import React
 import * as React from "react";
 
-// Step 2: Define your component
+import { Link } from "gatsby";
+/**
+ * * Link
+ *
+ * The Gatsby Link component provides a performance feature called preloading. This means that the
+ * resources for the linked page are requested when the link scrolls into view or when the mouse
+ * hovers on it. That way, when the user actually clicks on the link, the new page can load super
+ * quickly.
+ *
+ * Use the Link component for linking between pages within your site. For external links
+ * to pages not created by your Gatsby site, use the regular HTML <a> tag.
+ */
+
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   );
 };
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>;
 
-// Step 3: Export your component
 export default IndexPage;
