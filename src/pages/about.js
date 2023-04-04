b04ebcee3859237415263254a5d5a0b2b0dc6862
Christian Borja <cpborja32@gmail.com>
2023-04-04 16:22:25 -0700

    adds meta tag to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,19 @@ const AboutPage = () => {
  * Gatsby includes a built-in Head export that allows you to add elements to the document head of
  * your pages.
  */
-export const Head = () => <title>About Me</title>;
+export const Head = () => (
+  <>
+    <title>About Me</title>
+    {/**
+     * Meta tags, also known as metadata, are HTML snippets used in web development to describe the
+     * content on a web page. Meta tags don't appear on the pages but instead exist within the
+     * website's source code, also known as the HTML code, located in the website head. These tags
+     * provide search engines with descriptions about the page content to help them determine what
+     * the page is about and deliver the most relevant pages to search engine users.
+     */}
+    <meta name="description" content="Your description" />
+  </>
+);
 
 // Step 3: Export your component
 export default AboutPage;
