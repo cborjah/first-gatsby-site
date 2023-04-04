48e5c7cda02bcfaa52ca1f289a2f0c8d8b2b6c04
Christian Borja <cpborja32@gmail.com>
2023-04-04 16:32:46 -0700

    adds Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 // Step 1: Import React
 import * as React from "react";
-
 import { Link } from "gatsby";
 /**
  * * Link
@@ -14,12 +13,14 @@ import { Link } from "gatsby";
  * to pages not created by your Gatsby site, use the regular HTML <a> tag.
  */
 
+import Layout from "../components/layout";
+
 const IndexPage = () => {
   return (
     <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Layout pageTitle="Home Page">
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
     </main>
   );
 };
