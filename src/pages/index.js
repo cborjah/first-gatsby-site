0fbfaeb9eeb4c82c8b496a674603352726605220
Christian Borja <cpborja32@gmail.com>
2023-04-10 16:18:52 -0700

    adds notes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,20 @@ import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
+/**
+ * The filesystem source plugin lets you query data about files, but it doesn’t let you
+ * use the data inside the files themselves.
+ *
+ * To make this possible, Gatsby supports transformer plugins, which take the raw content from
+ * source plugins and transform it into something more usable.
+ *
+ * e.g. gatsby-plugin-mdx
+ *
+ * This plugin allows use of MDX, a file format that allows Markdown and JSX alongside your text
+ * content.
+ *
+ */
+
 const IndexPage = () => {
   return (
     <main>
