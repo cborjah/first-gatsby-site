3cc3fb61a54e02450fb0c2a9a25f110214d87df1
Christian Borja <cpborja32@gmail.com>
2023-04-10 15:24:19 -0700

    configures gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp` // Needed for dynamic images
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    }
   ]
 };
