488804291b1053c2005b764b899ca466aa6f0c73
Christian Borja <cpborja32@gmail.com>
2023-04-10 16:01:35 -0700

    adds blog posts, blog page now renders posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,33 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+        {data.allFile.nodes.map(node => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
+
+/**
+ * To pull data into a page component, use a page query instead of useStaticQuery. The result is
+ * automatically passed into the components defined in this file.
+ */
+export const query = graphql`
+  query {
+    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="My Blog Posts" />;
 
